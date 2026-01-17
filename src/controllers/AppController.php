00e9553931f78abe35847282fc59d71d92e178a4
mateusz <mateuszwojtasz1@gmail.com>
2026-01-17 16:49:24 +0100

    add bingo E1 - HTTPS

--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -1,7 +1,18 @@
 <?php
 
 
-class AppController {
+class AppController
+{
+
+    public function __construct()
+    {
+        //BINGO E1 – HTTPS
+        if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
+            header('HTTP/1.1 403 Forbidden');
+            echo "HTTPS required";
+            exit;
+        }
+    }
     protected function isGet(): bool
     {
         return $_SERVER["REQUEST_METHOD"] === 'GET';
@@ -11,19 +22,19 @@ class AppController {
     {
         return $_SERVER["REQUEST_METHOD"] === 'POST';
     }
- 
+
     protected function render(string $template = null, array $variables = [])
     {
-        $templatePath = 'public/views/'. $template.'.html';
+        $templatePath = 'public/views/' . $template . '.html';
         $templatePath404 = 'public/views/404.html';
         $output = "";
-                 
-        if(file_exists($templatePath)){
+
+        if (file_exists($templatePath)) {
             // ["message" => "Błędne hasło!"]
             extract($variables);
-           // $message = "Błędne hasło!"
-           // echo $message
-            
+            // $message = "Błędne hasło!"
+            // echo $message
+
             ob_start();
             include $templatePath;
             $output = ob_get_clean();
